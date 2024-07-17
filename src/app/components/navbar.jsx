394185ba5cfb45451e5412e5c2b5791fc61017b0
MatthewCCChang <deadwalker1228@gmail.com>
2024-07-16 19:56:57 -0700

    Fix navbar text center issue

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
-
+//still need to adjust the height of the links, maybe wrap with button?, try with padding for each li
 export default function Navbar() {
     return (
         <div className="flex justify-end  bg-red-500 w-screen h-20">
-            <ul className=" h-full bg-blue-400 flex-row"> 
-                <li className="h-full content-center inline mx-5 text-2xl">
+            <ul className=" pt-5 h-full bg-blue-400 flex-row"> 
+                <li className="h-full content-center inline mx-5 text-2xl "> 
                 <Link href="/" >Home</Link>
                 </li>
                 <li className="inline mx-5 text-2xl">
