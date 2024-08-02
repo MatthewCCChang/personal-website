1ce434f447a5175f65ede11a2fdf3b0a88c2a359
MatthewCCChang <deadwalker1228@gmail.com>
2024-08-01 23:21:42 -0700

    Added footer

---
+++ src/app/components/footer.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Footer = function () {
+    return(
+        <div>
+            <Link>LinkedIn</Link>
+            <Link>Github</Link>
+            <Link>Email</Link>
+        </div>
+    )
+}
+
+export default Footer;
