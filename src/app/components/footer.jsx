a2416c598c01b1eb8b925f08eed830acfa77d0a8
MatthewCCChang <deadwalker1228@gmail.com>
2024-08-02 18:20:28 -0700

    Image buttons on footer

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,11 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Footer = function () {
     return(
-        <div>
-            <Link>LinkedIn</Link>
-            <Link>Github</Link>
-            <Link>Email</Link>
+        <div className=" flex justify-center w-full">
+            <div className="ml-3 mr-3 ">
+                
+                <Link href="https://www.linkedin.com/in/matthew-chang-39754a216/">
+                <Image src="/linkedin.png" width={20} height={20} alt="image of linkedin"/>
+                </Link>
+            </div>
+            <div className="ml-3 mr-3 ">
+            <Link href="#">
+                <Image src="/github.png" width={20} height={20} alt="image of github"/>
+            </Link>
+            </div>
+            <div className="ml-3 mr-3 ">
+            <Link href="#">Email</Link>
+            </div>
+            
         </div>
     )
 }
