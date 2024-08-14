fd57456160f19e83ed26b4bcdd8c3f7f7d648328
MatthewCCChang <deadwalker1228@gmail.com>
2024-08-13 21:28:10 -0700

    Add photo next to text

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import Navbar from "@/app/components/navbar";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -8,12 +8,18 @@ export default function Home() {
     //space themed
     <div className="  mb-12 flex flex-row justify-center flex-wrap h-screen bg-black w-full">
       {/* <Navbar/> */}
-      <div className="flex text-center text-white-500 mt-5 font-mono">
+      <div className="flex flex-row text-white-500 mt-5 font-mono h-[50%] bg-red-500">
+        <div className="flex basis-3/4 bg-blue-600">
+          <h1 className="m-3">
+            Welcome to my website. I'm Matthew Chang, student and software
+            developer with experience in full stack, machine learning, and DevOps.
+          </h1>
+        </div>
+        <div className="basis-1/4 m-2">
+          <Image src={'/Profile.jpg'} width={400} height={400} alt='Image of Matthew Chang'/>
+        </div>
         {/*image*/}
-        <h1>
-          Welcome to my website. I'm Matthew Chang, student and software
-          developer with experience in full stack, machine learning, and DevOps.
-        </h1>
+        
         {/*display number of projects and commits etc*/}
       </div>
     </div>
