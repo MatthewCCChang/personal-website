03e5065bfa838e1efd14377862d04a09c5d41edd
MatthewCCChang <deadwalker1228@gmail.com>
2024-08-17 21:58:08 -0700

    Setup card basic structure

---
+++ src/app/components/cards.jsx
@@ -0,0 +1,17 @@
+
+
+export default function Cards() {
+    return(
+        <div>
+            <div>
+
+            </div>
+            <div>
+                <p></p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
