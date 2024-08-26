3f783a044313c46db76e9877daa75d44e0e06f4b
MatthewCCChang <deadwalker1228@gmail.com>
2024-08-26 16:02:32 -0700

    Added customization to cards

--- src/app/components/cards.jsx
+++ src/app/components/cards.jsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Cards({img, title, description, github}) {
+export default function Cards({img, title, description, github, background}) {
+    const colors = {
+        "white": "bg-white text-black w-[32%] my-5 mx-2 shadow-md rounded-md shadow-white overflow-hidden",
+        "blue": "bg-blue-500 text-white w-[32%] my-5 mx-2 shadow-md rounded-md shadow-blue-500 overflow-hidden"
+    }
     return(
-        <div className="bg-white text-black w-[32%] my-5 mx-2 shadow-lg rounded">
+        <div className={colors[background]}>
             {/* <div className="relative">
              <Image src={img} layout="fill" objectFit="cover" alt='img'></Image> 
             </div> */}
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,12 +2,15 @@ import Cards from "../components/cards"
 
 export default function Projects() {
     return (
-        <div className="justify-evenly flex flex-wrap bg-red-600">
+        <div className="justify-evenly flex flex-wrap ">
             <Cards img='/background.jpg' title='portfolio' description={'this is my portfolio'} 
-            github={'https://github.com/MatthewCCChang/personal-website'}></Cards>
-            <Cards img='/night-background.jpg' title='portfolio' description={'this is my portfolio'} github={'https://github.com/MatthewCCChang/personal-website'}></Cards>
-            <Cards img='/background.jpg' title='portfolio' description={'this is my portfolio'} github={'https://github.com/MatthewCCChang/personal-website'}></Cards>
-            <Cards img='/night-background.jpg' title='portfolio' description={'this is my portfolio'} github={'https://github.com/MatthewCCChang/personal-website'}></Cards>
+            github={'https://github.com/MatthewCCChang/personal-website'} background={"white"}></Cards>
+            <Cards img='/night-background.jpg' title='portfolio' description={'this is my portfolio'} github={'https://github.com/MatthewCCChang/personal-website'}
+            background={"blue"}></Cards>
+            <Cards img='/background.jpg' title='portfolio' description={'this is my portfolio'} github={'https://github.com/MatthewCCChang/personal-website'}
+            background={"white"}></Cards>
+            <Cards img='/night-background.jpg' title='portfolio' description={'this is my portfolio'} github={'https://github.com/MatthewCCChang/personal-website'}
+            background={"white"}></Cards>
         </div>
     )
 }
