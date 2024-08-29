d407931036f66daba5ffeca5409a1befe0a1ca68
MatthewCCChang <deadwalker1228@gmail.com>
2024-08-28 19:40:23 -0700

    Added information about project

--- src/app/components/cards.jsx
+++ src/app/components/cards.jsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 export default function Cards({img, title, description, github, background}) {
     const colors = {
         "white": "bg-white text-black w-[32%] my-5 mx-2 shadow-md rounded-md shadow-white overflow-hidden",
-        "blue": "bg-blue-500 text-white w-[32%] my-5 mx-2 shadow-md rounded-md shadow-blue-500 overflow-hidden"
+        "blue": "bg-blue-500 text-white w-[32%] my-5 mx-2 shadow-md rounded-md shadow-blue-500 overflow-hidden ",
+        "purple": "bg-purple-400 text-white w-[32%] my-5 mx-2 shadow-md rounded-md shadow-blue-500 overflow-hidden "
+
     }
     return(
         <div className={colors[background]}>
             {/* <div className="relative">
              <Image src={img} layout="fill" objectFit="cover" alt='img'></Image> 
             </div> */}
-            <img class="w-full" src={img} alt="Sunset in the mountains"></img>
+            <img className="w-full object-cover aspect-video" src={img} alt="image"></img>
             <div>
                 <h1>{title}</h1>
             </div>
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,16 +1,17 @@
 import Cards from "../components/cards"
 
 export default function Projects() {
+    //colro scheme: white=large projects, light-blue=hackathon projects, blue-purple: personal
     return (
         <div className="justify-evenly flex flex-wrap ">
-            <Cards img='/background.jpg' title='portfolio' description={'this is my portfolio'} 
-            github={'https://github.com/MatthewCCChang/personal-website'} background={"white"}></Cards>
-            <Cards img='/night-background.jpg' title='portfolio' description={'this is my portfolio'} github={'https://github.com/MatthewCCChang/personal-website'}
+            <Cards img='/MLH.png' title='MLH x Meta Fellowship' description={'This is the portfolio created for this production engineering fellowshi['} 
+            github={'https://github.com/MatthewCCChang/MLH_Portfolio'} background={"white"}></Cards>
+            <Cards img='/Luna_Gym.png' title='Luna Gym' description={'Venus Hacks project focused on finding female-friendly gyms'} github={'https://github.com/MatthewCCChang/VenusHack'}
             background={"blue"}></Cards>
-            <Cards img='/background.jpg' title='portfolio' description={'this is my portfolio'} github={'https://github.com/MatthewCCChang/personal-website'}
+            <Cards img='/hangman (3).png' title='Hangman Poker' description={'A fusion game of Hangman and Texas Holdem'} github={'https://github.com/MatthewCCChang/Hangman-Poker'}
             background={"white"}></Cards>
-            <Cards img='/night-background.jpg' title='portfolio' description={'this is my portfolio'} github={'https://github.com/MatthewCCChang/personal-website'}
-            background={"white"}></Cards>
+            <Cards img='/Whats_The_Move.png' title="What's the Move" description={'LA Hacks project centered around encouraging outdorr activites and adventures'} github={'https://github.com/MatthewCCChang/Whats_The_Move'}
+            background={"blue"}></Cards>
         </div>
     )
 }
