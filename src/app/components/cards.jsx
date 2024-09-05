0394a387eadd04764d6d2378a72b7b6291e44800
MatthewCCChang <deadwalker1228@gmail.com>
2024-09-04 19:22:56 -0700

    Uploaded projects to page

--- src/app/components/cards.jsx
+++ src/app/components/cards.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Cards({img, title, description, github, background}) {
     const colors = {
-        "white": "bg-white text-black w-[32%] my-5 mx-2 shadow-md rounded-md shadow-white overflow-hidden",
+        "magenta": "bg-pink-600 text-white w-[32%] my-5 mx-2 shadow-md rounded-md shadow-pink-700 overflow-hidden",
         "blue": "bg-blue-500 text-white w-[32%] my-5 mx-2 shadow-md rounded-md shadow-blue-500 overflow-hidden ",
-        "purple": "bg-purple-400 text-white w-[32%] my-5 mx-2 shadow-md rounded-md shadow-blue-500 overflow-hidden "
+        "purple": "bg-purple-400 text-white w-[32%] my-5 mx-2 shadow-md rounded-md shadow-purple-500 overflow-hidden "
 
     }
     return(
@@ -14,13 +14,13 @@ export default function Cards({img, title, description, github, background}) {
              <Image src={img} layout="fill" objectFit="cover" alt='img'></Image> 
             </div> */}
             <img className="w-full object-cover aspect-video" src={img} alt="image"></img>
-            <div>
+            <div className="px-1 text-lg">
                 <h1>{title}</h1>
             </div>
-            <div>
+            <div className="px-1">
                 <p>{description}</p>
             </div>
-            <div>
+            <div className="hover:underline px-1">
                 <Link href={github}>View</Link>
             </div>
         </div>
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -4,14 +4,29 @@ export default function Projects() {
     //colro scheme: white=large projects, light-blue=hackathon projects, blue-purple: personal
     return (
         <div className="justify-evenly flex flex-wrap ">
-            <Cards img='/MLH.png' title='MLH x Meta Fellowship' description={'This is the portfolio created for this production engineering fellowshi['} 
-            github={'https://github.com/MatthewCCChang/MLH_Portfolio'} background={"white"}></Cards>
+            <Cards img='/MLH.png' title='MLH x Meta Fellowship' description={'This is the portfolio created for this production engineering fellowship'} 
+            github={'https://github.com/MatthewCCChang/MLH_Portfolio'} background={"magenta"}></Cards>
             <Cards img='/Luna_Gym.png' title='Luna Gym' description={'Venus Hacks project focused on finding female-friendly gyms'} github={'https://github.com/MatthewCCChang/VenusHack'}
             background={"blue"}></Cards>
             <Cards img='/hangman (3).png' title='Hangman Poker' description={'A fusion game of Hangman and Texas Holdem'} github={'https://github.com/MatthewCCChang/Hangman-Poker'}
-            background={"white"}></Cards>
+            background={"magenta"}></Cards>
             <Cards img='/Whats_The_Move.png' title="What's the Move" description={'LA Hacks project centered around encouraging outdorr activites and adventures'} github={'https://github.com/MatthewCCChang/Whats_The_Move'}
             background={"blue"}></Cards>
+            <Cards img='/Hephaestus.png' title="Hephaestus" description={'Project for BRDG Sustainability Challenge aimed to tackle river pollution problems'} github={'https://github.com/alejandro-garf/Hephaestus-Webapp'}
+            background={"purple"}></Cards>
+            
+            <Cards img='/sleep_tracker.png' title="Sleep Tracker" description={'Project that measures sleep quality and activities.'} github={'https://github.com/uci-inf-133/a4-sleep-tracker-fullstackdemonz'}
+            background={"purple"}></Cards>
+            <Cards img='/hand_gesture.png' title="Hand Gesture" description={'Project that provides various imaging options through hand gestures detected by AI.'} github={'https://github.com/uci-inf-133/a5-final-project-finalteam'}
+            background={"purple"}></Cards>
+            
+            <Cards img='/diabetes.png' title="Diabetes" description={'Analysis of diabetes patients using supervised learning.'} github={'https://github.com/MatthewCCChang/Diabetes-Dataset'}
+            background={"purple"}></Cards>
+            <Cards img='/search.png' title="Search" description={'A search engine for department specific webapges.'} github={'https://github.com/MatthewCCChang/Pickle-Search'}
+            background={"purple"}></Cards>
+            <Cards img='/cocktail.png' title="Cocktail" description={'A website that filters cocktails based off of user needs.'} github={'https://github.com/MatthewCCChang/Cocktail'}
+            background={"magenta"}></Cards>
+            
         </div>
     )
 }
